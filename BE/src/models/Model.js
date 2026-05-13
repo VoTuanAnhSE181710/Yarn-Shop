c9c9a408f3ae8a4c1134ab8c7916814cb7fa9caf
Kamui <tranngoc5979@gmail.com>
2026-05-13 08:09:55 +0700

    no BE

--- BE/src/models/Model.js
+++
@@ -1,6 +0,0 @@
-import User from "./user.js";
-import RefreshToken from "./RefreshToken.js";
-import Role from "./role.js";
-import Permission from "./permission.js";
-
-export { User, Role, RefreshToken, Permission };
