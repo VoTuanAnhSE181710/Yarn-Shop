cda336d58067e8c7a0179b03b2d66b626a0ec38e
Võ Tuấn Anh <anhvtse181710@fpt.edu.vn>
2026-07-27 20:49:39 +0700

    Fix case sensitivity issue for Message and Conversation model imports

--- BE/src/models/Model.js
+++ BE/src/models/Model.js
@@ -13,7 +13,7 @@ import Order from "./order.js";
 import OrderReport from "./orderReport.js";
 import SupportDIY from "./supportDIY.js";
 import Address from "./address.js";
-import Message from "./Message.js";
-import Conversation from "./Conversation.js";
+import Message from "./message.js";
+import Conversation from "./conversation.js";
 
 export { User, Role, RefreshToken, Permission, Category, Video, Product, Course, Lesson, Kit, DIYPost, Order, OrderReport, SupportDIY, Address, Message, Conversation };
