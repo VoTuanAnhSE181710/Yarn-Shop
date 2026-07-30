3347393623acabe1807cc75397411b9ab64d2e35
Võ Tuấn Anh <anhvtse181710@fpt.edu.vn>
2026-07-30 19:08:37 +0700

    fix: Update reset password email link to point to /reset-password route

--- BE/src/services/mail.service.js
+++ BE/src/services/mail.service.js
@@ -135,7 +135,7 @@ class MailService {
 
         const resetPasswordAccessKey = `reset_password_access:${uuid}`;
 
-        const resetPasswordLink = `${process.env.FRONTEND_URL}?uuid=${uuid}`
+        const resetPasswordLink = `${process.env.FRONTEND_URL}/reset-password?uuid=${uuid}`
 
         const mailOptions = generateForgotPasswordOptions(normalizedEmail, existingUser.fullName, resetPasswordLink)
 
